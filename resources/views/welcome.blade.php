443a8cb0f9818ed92a6994c08e0d4492afec7bc1
Roberto Sierra <roberto30589@gmail.com>
2020-12-29 21:59:35 -0300

    Inicio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GRAN RIFA SOLIDARIA A JOAQUIN MORAN </title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #4A2340;
+                color: #F9D564;
+                margin: 0;
+            }
+            .titulo {
+                letter-spacing:0.2em;
+                font-family: 'Fredoka One', cursive;
+                font-weight: 200;
+                font-size: 10vw;
+                text-transform: uppercase;
+                text-align:center;
+            }
+            .subtitulo {
+                letter-spacing:0.2em;
+                font-family: 'Fredoka One', cursive;
+                font-weight: 200;
+                font-size: 4vw;
+                text-transform: uppercase;
+                text-align:center;
+            }
+            .container{
+                padding-left: 8%;
+                padding-right: 8%;
+            }
+
+            .informacion{
+                color:beige;
+                letter-spacing:1.2px;
+                font-family: 'Poppins', sans-serif;
+                text-transform: uppercase;
+                font-weight: 200;
+            }
+            .informacion p{
+                margin: 0;
+            }
+            
+            @media screen and (min-width: 800px) {
+
+                .titulo{
+                    font-size: 85px;
+                }
+
+                .subtitulo {
+                    font-size: 32px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="titulo">GRAN RIFA</h1>
+            <h4 class="subtitulo">A BENEFICIO DE LA REHABILITACIÓN DE JOAQUIN MORAN QUIEN SE ENCUENTRA CON LESIÓN MEDULAR </h4>
+            <h4 class="subtitulo">VALOR: $2.000 SORTEO 05 FEBRERO 2021</h4>
+
+            <div align="left" style="font-size:1rem" class="informacion">
+                <h3>La cuenta oficial para recaudar el dinero es:</h3>
+                <p>Cuenta de Ahorro Banco Estado</p>
+                <p>Nº: 36761766109</p>
+                <p>NOMBRE: LESLIE MARÍN PÉREZ</p>
+                <p>RUT: 16.280.312-3</p>
+                <p>EMAIL: LESLIEMARIN.18@GMAIL.COM</p>
+                <p><b>P.D:</b> Al depositar, por favor, indicar en el comentario el número de rifa asociado</p>
+            </div>
+            <div class="informacion" align="center">
+            </div>            
+            <h4 class="subtitulo">MÁS INFORMACIÓN AL: +56958379948 O EN NUESTRA FANPAGE DE FACEBOOK "JOACOTON"</h4>
+        </div>
+    </body>
+</html>
